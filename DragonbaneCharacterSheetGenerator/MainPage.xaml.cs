55549e0e19b27c21b8b15bc4b63f588f8f3bcb4f
Bas Trebus <bas.trebus@maastrichtuniversity.nl>
2025-10-10 17:29:05 +0200

    Add project files.

---
+++ DragonbaneCharacterSheetGenerator/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DragonbaneCharacterSheetGenerator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
