d1f5ff85c9a33baa03a60bc67860640bf6d76473
Bas Trebus <140490930+BasTrebus@users.noreply.github.com>
2025-10-10 18:40:20 +0200

    Create exampleHeader.cs

---
+++ exampleHeader.cs
@@ -0,0 +1,26 @@
+/*
+ * Project: Dragonbane Helper (Unofficial)
+ * File: [YourFileName].cs
+ * Author: [Your Name]
+ * License: MIT (see LICENSE file for details)
+ *
+ * ------------------------------------------------------------
+ * Dragonbane Compatibility Notice
+ *
+ * This file is part of an independent, open source project 
+ * designed for use with the Dragonbane roleplaying game, 
+ * published by Free League Publishing.
+ *
+ * - This code does NOT include or reproduce any copyrighted 
+ *   text, artwork, or lore from the Dragonbane rulebooks.
+ * - All Dragonbane trademarks, logos, and copyrighted material 
+ *   remain the property of Free League Publishing.
+ * - This project is not affiliated with, endorsed, sponsored, 
+ *   or specifically approved by Free League Publishing.
+ * - References to Dragonbane rules and terminology are made 
+ *   under Free League’s Third-Party Tabletop Module License, 
+ *   and require ownership of the official Dragonbane Core 
+ *   Rulebook to play.
+ *
+ * ------------------------------------------------------------
+ */
